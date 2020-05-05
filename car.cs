4554df82bb1bd9c4ff76707fe6121c926b955cfd
nitunwa <nitun.2@gmail.com>
2020-05-05 14:42:24 -0700

    update worth buying method

--- car.cs
+++ car.cs
@@ -15,10 +15,18 @@ public class Car
     Miles = miles;
     Message = message;
   }
-  public bool WorthBuying(int maxPrice)
+  public bool WorthBuying(int maxPrice,int maxMiles)
   {
-    return (Price < maxPrice);
+    return (Price < maxPrice && Miles> maxMiles);
   }
+
+  public  bool DoesNotMatch(int maxPrice,int minPrice){
+     if(maxPrice<minPrice){
+       return true;
+     }
+     return false;
+  }
+
   public void toString()
   {
 
@@ -42,7 +50,7 @@ public class Program
 
 
   }
-
+  
   public static void Main()
   {
     Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, " Don't forget to check out our used cars");
@@ -57,12 +65,36 @@ public class Program
     Console.WriteLine("Enter maximum price: ");
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
+    bool flag = false;
+	  int minPrice = Int32.MaxValue;
+
+    foreach(Car car in Cars){
+	  if(car.Price < minPrice){
+		minPrice = car.Price;  
+	  }	
+      flag = car.DoesNotMatch(maxPrice,minPrice);
+      
+    }
+    if(flag){
+      Console.WriteLine("No cars match the budget.Would you like to check others [Yes for y and no for n].");
+      string answer = Console.ReadLine();
+      if(answer == "y"){
+        Main();
+      }else{
+        Console.WriteLine("Good Bye");
+      }
+    }
+    
+
+    Console.WriteLine("Enter maximum miles: ");
+    string stringMaxMiles = Console.ReadLine();
+    int maxMiles = int.Parse(stringMaxMiles);
 
     List<Car> CarsMatchingSearch = new List<Car>(0);
 
     foreach (Car automobile in Cars)
     {
-      if (automobile.WorthBuying(maxPrice))
+      if (automobile.WorthBuying(maxPrice, maxMiles))
       {
         CarsMatchingSearch.Add(automobile);
       }
